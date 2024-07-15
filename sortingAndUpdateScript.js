dff8d78ff922bae94c8d8cbba571763cfa18f902
samgee-dev <samgee.dev@gmail.com>
2024-07-15 16:55:02 +1000

    Added checks for old information
    Also updated some old constraints.

--- sortingAndUpdateScript.js
+++ sortingAndUpdateScript.js
@@ -60,10 +60,10 @@ function subCompanyUpdate() {
   var arcNames = subCompanySheet.getRange('D60:D61')
 
   // Variables
-  var com1Removed = sortingSheet.getRange('L5:L10')
-  var com2Removed = sortingSheet.getRange('L12:L17')
-  var com3Removed = sortingSheet.getRange('L19:L24')
-  var arcRemoved = sortingSheet.getRange('L26:L27')
+  var com1Removed = sortingSheet.getRange('M5:M10')
+  var com2Removed = sortingSheet.getRange('M12:M17')
+  var com3Removed = sortingSheet.getRange('M19:M24')
+  var arcRemoved = sortingSheet.getRange('M26:M27')
   var comSlots = 6
   var arcSlots = 2
   
@@ -76,9 +76,9 @@ function subCompanyUpdate() {
   subCompanySort()
 
   // Add variables
-  var toAdd = sortingSheet.getRange('K5:K29')
+  var toAdd = sortingSheet.getRange('L5:L29')
   var companies = sortingSheet.getRange('B36:B39')
-  var names = sortingSheet.getRange('J5:J29')
+  var names = sortingSheet.getRange('K5:K29')
   var slots = 25
   
   // Do adds
@@ -140,8 +140,8 @@ function taskSort() {
   const ss = SpreadsheetApp.getActiveSpreadsheet()
   var taskSheet = ss.getSheetByName("Task Checklist")
   var sortSheet = ss.getSheetByName("Sorting Code")
-  var sortSpec = sortSheet.getRange('H4:H13')
-  var sortSerg = sortSheet.getRange('H15:H26')
+  var sortSpec = sortSheet.getRange('H4:H9')
+  var sortSerg = sortSheet.getRange('H12:H23')
   var sortWO = sortSheet.getRange('I4:I10')
 
 
@@ -149,50 +149,99 @@ function taskSort() {
   var taskSortColumn = 23
   var taskFirstColumn = 3
   var numberOfColumns = 21
-  var specSlots = 10 // Stupid merge cell so +1
+
+  var specSlots = 6
+  var specFirstRow = 8
+  var specLastRow = 13
 
   var sergSlots = 12
-  var sergFirstRow = 23
-  var sergLastRow = 34
+  var sergFirstRow = 19
+  var sergLastRow = 30
+
   var woSlots = 7
-  var woFirstRow = 40
-  var woLastRow = 46
+  var woFirstRow = 36
+  var woLastRow = 42
 
-  // Make a new table to sort for spec
-  var specTable = taskSheet.getRange('C8:V17')
-  var newTableFirstC = 4
-  var newTableLastC = 23
-  var newTableFirstR = 36
-  var newTableLastR = 45
-  var specSortColumn = 24
+  // Commented out old dupe table code when the table row was merged. Might be useful in the future.
 
-  // Sort specialist in temp
-  specTable.copyValuesToRange(sortSheet, newTableFirstC, newTableLastC, newTableFirstR, newTableLastR)
+  // // Make a new table to sort for spec
+  // var specTable = taskSheet.getRange('C8:V17')
+  // var newTableFirstC = 4
+  // var newTableLastC = 23
+  // var newTableFirstR = 36
+  // var newTableLastR = 45
+  // var specSortColumn = 24
+
+  // // Sort specialist in temp
+  // specTable.copyValuesToRange(sortSheet, newTableFirstC, newTableLastC, newTableFirstR, newTableLastR)
 
   // Sort the rest
-  sort(sortSheet, sortSpec, specSortColumn, newTableFirstR, newTableLastR, specSlots, newTableFirstC, numberOfColumns)
+  // sort(sortSheet, sortSpec, specSortColumn, newTableFirstR, newTableLastR, specSlots, newTableFirstC, numberOfColumns)
+  sort(taskSheet, sortSpec, taskSortColumn, specFirstRow, specLastRow, specSlots, taskFirstColumn, numberOfColumns)
   sort(taskSheet, sortSerg, taskSortColumn, sergFirstRow, sergLastRow, sergSlots, taskFirstColumn, numberOfColumns)
   sort(taskSheet, sortWO, taskSortColumn, woFirstRow, woLastRow, woSlots, taskFirstColumn, numberOfColumns)
 
-  // Transfer temp spec table to actual (Two parts since merge)
-  var sortedTempSpecTable1 = sortSheet.getRange('D36:D41')
-  var sortedTempSpecTable1p2 = sortSheet.getRange('H36:W41')
-  var sortedTempSpecTable2 = sortSheet.getRange('D42:D44')
-  var sortedTempSpecTable2p2 = sortSheet.getRange('H42:W44')
+  // // Transfer temp spec table to actual (Two parts since merge)
+  // var sortedTempSpecTable1 = sortSheet.getRange('D36:D41')
+  // var sortedTempSpecTable1p2 = sortSheet.getRange('H36:W41')
+  // var sortedTempSpecTable2 = sortSheet.getRange('D42:D44')
+  // var sortedTempSpecTable2p2 = sortSheet.getRange('H42:W44')
 
-  // (Some variables got from above)
-  var specFirstRow = 8
-  var specLastRow = 17
-  var taskLastColumn = 22
-  var mergedRow = 13
-  var rowAfterMerge = 15
-  var columnAfterCode = 7 // No before since same as name cell
+  // // (Some variables got from above)
+  // var specFirstRow = 8
+  // var specLastRow = 17
+  // var taskLastColumn = 22
+  // var mergedRow = 13
+  // var rowAfterMerge = 15
+  // var columnAfterCode = 7 // No before since same as name cell
 
-  // Also have to avoid copying the middle cells as code won't transfer
-  sortedTempSpecTable1.copyValuesToRange(taskSheet, taskFirstColumn, taskFirstColumn, specFirstRow, mergedRow)
-  sortedTempSpecTable1p2.copyValuesToRange(taskSheet, columnAfterCode, taskLastColumn, specFirstRow, mergedRow)
-  sortedTempSpecTable2.copyValuesToRange(taskSheet, taskFirstColumn, taskFirstColumn, rowAfterMerge, specLastRow)
-  sortedTempSpecTable2p2.copyValuesToRange(taskSheet, columnAfterCode, taskLastColumn, rowAfterMerge, specLastRow)
+  // // Also have to avoid copying the middle cells as code won't transfer
+  // sortedTempSpecTable1.copyValuesToRange(taskSheet, taskFirstColumn, taskFirstColumn, specFirstRow, mergedRow)
+  // sortedTempSpecTable1p2.copyValuesToRange(taskSheet, columnAfterCode, taskLastColumn, specFirstRow, mergedRow)
+  // sortedTempSpecTable2.copyValuesToRange(taskSheet, taskFirstColumn, taskFirstColumn, rowAfterMerge, specLastRow)
+  // sortedTempSpecTable2p2.copyValuesToRange(taskSheet, columnAfterCode, taskLastColumn, rowAfterMerge, specLastRow)
+}
+
+function taskUpdate() {
+  // Set up sheets to be used
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  var taskSheet = ss.getSheetByName("Task Checklist")
+  var sortingSheet = ss.getSheetByName("Sorting Code")
+  var specNames = taskSheet.getRange('C8:C13')
+  var sergNames = taskSheet.getRange('C19:C30')
+  var woNames = taskSheet.getRange('C36:C42')
+
+  // Variables
+  var specRemoved = sortingSheet.getRange('P5:P10')
+  var sergRemoved = sortingSheet.getRange('P12:P23')
+  var woRemoved = sortingSheet.getRange('Q5:Q11')
+  var specList = taskSheet.getRange('G8:V13')
+  var sergList = taskSheet.getRange('G19:V30')
+  var woList = taskSheet.getRange('G36:V42')
+  var numberOfColumns = 16
+
+  var specSlots = 6
+  var sergSlots = 12
+  var woSlots = 7
+  
+  // Do removals
+  removeCompany(specNames, specRemoved, specSlots, specList, numberOfColumns)
+  removeCompany(sergNames, sergRemoved, sergSlots, sergList, numberOfColumns)
+  removeCompany(woNames, woRemoved, woSlots, woList, numberOfColumns)
+
+  // Sort
+  taskSort()
+
+  // Add variables
+  var toAdd = sortingSheet.getRange('O5:O29')
+  var names = sortingSheet.getRange('K5:K29')
+  var slots = 25
+  
+  // Do adds
+  addTask(names, toAdd, slots, specNames, sergNames, woNames, specSlots, sergSlots, woSlots)
+
+  // Sort
+  taskSort()
 }
 
 function sort(sheet, sortColumn, newSortColumn, firstRow, lastRow, numberOfRows, firstColumn, numberOfColumns) {
@@ -239,6 +288,45 @@ function addCompany(names, addList, companies, slots, com1Names, com2Names, com3
           arcNames.getCell(arcCount, 1).setValue(name)
           arcCount--
           break;
+      }
+    }
+  }
+}
+
+function removeTask(taskNames, taskRemoved, slots, table, numberOfColumns) {
+  for (var i = 1; i < slots + 1; i++) {
+    if (taskRemoved.getCell(i, 1).getValue() === false) {
+      taskNames.getCell(i, 1).clearContent()
+    }
+
+    // Check this later!
+    for (var j = 1; j < numberOfColumns + 1; j++) {
+      taskNames.getCell(i, 1).clearContent()
+      table.getCell(i, j).setValue("FALSE")
+    }
+  }
+}
+
+function addTask(names, addList, slots, specNames, sergNames, woNames, specSlots, sergSlots, woSlots) {
+  // Loop through each slot and add to the list if necessary
+  for (var i = 1; i < slots + 1; i++) {
+    if (!addList.getCell(i, 1).isBlank()) {
+      var rankNumber = addList.getCell(i, 1).getValue()
+      var name = names.getCell(i, 1).getValue()
+
+      if (rankNumber == 5) {
+        specNames.getCell(specSlots, 1).setValue(name)
+        specSlots--
+      }
+
+      else if (rankNumber < 13 && rankNumber > 5) {
+        sergNames.getCell(sergSlots, 1).setValue(name)
+        sergSlots--
+      }
+
+      else if (rankNumber == 13) {
+        woNames.getCell(woSlots, 1).setValue(name)
+        woSlots--
       }
     }
   }
